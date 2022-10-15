ea3fabb6430dbb63e5ec81650e80fbd72b8b18fe
James Manning <jamesmanningr98@gmail.com>
2022-10-15 22:07:52 +0100

    Added selected departure functionality tests

--- src/stores/__tests__/departures.spec.ts
+++ src/stores/__tests__/departures.spec.ts
@@ -1,8 +1,9 @@
 import { PiniaVuePlugin, createPinia } from 'pinia'
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, beforeEach } from 'vitest'
 import Vue from 'vue'
 
 import { useDeparturesStore } from '../departures'
+import { exampleDepartures, exampleDeparture } from './testData'
 
 Vue.use(PiniaVuePlugin)
 
@@ -11,17 +12,84 @@ const app = new Vue({
 })
 
 describe('DeparturesStore', () => {
-  describe('State', () => {
+  describe('Departures', () => {
     it('Contains a list of departures', () => {
       const store = useDeparturesStore()
       expect(store.departures).toBeDefined()
     })
-  })
 
-  describe('Actions', () => {
     it('Has a method for fetching departures', () => {
       const store = useDeparturesStore()
       expect(store.fetchDepartures).toBeDefined()
+    })
+  })
+
+  describe('Selected departure', () => {
+    let store
+
+    beforeEach(() => {
+      store = useDeparturesStore()
+      store.departures = exampleDepartures
+    })
+
+    it('Contains a selected departure', () => {
+      const store = useDeparturesStore()
+      expect(store.selectedDeparture).toBeDefined()
+    })
+
+    it('Has a method for selecting a departure', () => {
+      const store = useDeparturesStore()
+      expect(store.selectDeparture).toBeDefined()
+    })
+
+    it('When selecing a departure, if the departure is in the departures the selected departure is set', () => {
+      const store = useDeparturesStore()
+      store.selectDeparture(exampleDeparture.flightNumber)
+
+      expect(store.selectedDeparture).toEqual(exampleDeparture)
+    })
+
+    it('When selecing a departure, if the departure is not in the departures the selected departure is not set', () => {
+      const store = useDeparturesStore()
+      store.selectDeparture('not a flight number')
+
+      expect(store.selectedDeparture).toBeNull()
+    })
+  })
+
+  describe('Updating a departure', () => {
+    let store
+
+    beforeEach(() => {
+      store = useDeparturesStore()
+      store.departures = exampleDepartures
+    })
+
+    it('Has a method for updating a departure', () => {
+      const store = useDeparturesStore()
+      expect(store.updateDeparture).toBeDefined()
+    })
+
+    it('When updating a departure, if the departure is in the departures the departure is updated', () => {
+      const store = useDeparturesStore()
+      store.departures = exampleDepartures
+      const updatedDeparture = { ...exampleDeparture, status: 'Example text' }
+      store.updateDeparture(updatedDeparture)
+
+      expect(store.departures).toContainEqual(updatedDeparture)
+    })
+
+    it('When updating a departure, if the departure is not in the departures the departure is not updated', () => {
+      const store = useDeparturesStore()
+      store.departures = exampleDepartures
+      const updatedDeparture = {
+        ...exampleDeparture,
+        flightNumber: 'not a flight number',
+        status: 'Example text',
+      }
+      store.updateDeparture(updatedDeparture)
+
+      expect(store.departures).not.toContainEqual(updatedDeparture)
     })
   })
 })
