79ecbc69ae90616464a0911c83e36e0b8ae02e78
James Manning <jamesmanningr98@gmail.com>
2022-10-15 23:14:01 +0100

    Added status type to departurestatus

--- src/components/departures/departure/DepartureStatus.vue
+++ src/components/departures/departure/DepartureStatus.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+
+import { inferStatusType } from '@/common/utils';
+
 const props = defineProps<{
   status: string
 }>()
+
+const type = computed(() => inferStatusType(props.status))
 </script>
 
 <template>
