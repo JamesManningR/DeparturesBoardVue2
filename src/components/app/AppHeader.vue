ea3e040d27c8e7bf63bcea58f642a85e95b2608d
James Manning <jamesmanningr98@gmail.com>
2022-10-15 17:57:51 +0100

    Created the header

---
+++ src/components/app/AppHeader.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import IconTakeoff from '../icons/IconTakeoff.vue'
+</script>
+
+<template>
+  <header
+    class="from-primary to-primaryDark flex w-full items-center gap-2 bg-gradient-to-t text-3xl text-black"
+  >
+    <div class="container mx-auto flex gap-2">
+      <IconTakeoff role="presentation" />
+      <h1>Departures</h1>
+    </div>
+  </header>
+</template>
+
+<style lang="scss"></style>
