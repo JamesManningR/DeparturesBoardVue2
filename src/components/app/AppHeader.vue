fde9641e223752227babef591d5daeb2bf5c29b9
James Manning <jamesmanningr98@gmail.com>
2022-10-15 18:12:26 +0100

    Styled header

--- src/components/app/AppHeader.vue
+++ src/components/app/AppHeader.vue
@@ -6,8 +6,8 @@ import IconTakeoff from '../icons/IconTakeoff.vue'
   <header
     class="from-primary to-primaryDark flex w-full items-center gap-2 bg-gradient-to-t text-3xl text-black"
   >
-    <div class="container mx-auto flex gap-2">
-      <IconTakeoff role="presentation" />
+    <div class="container mx-auto flex content-center gap-2">
+      <IconTakeoff role="presentation" class="w-fs h-fs" />
       <h1>Departures</h1>
     </div>
   </header>
