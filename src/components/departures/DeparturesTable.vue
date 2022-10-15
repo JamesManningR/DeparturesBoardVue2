1836eee2af9cb95c16650557a2cc96232f258e39
James Manning <jamesmanningr98@gmail.com>
2022-10-15 19:35:08 +0100

    Styled table on desktop

--- src/components/departures/DeparturesTable.vue
+++ src/components/departures/DeparturesTable.vue
@@ -1,28 +1,30 @@
 <script setup lang="ts"></script>
 
 <template>
-  <table>
-    <thead>
-      <tr>
-        <th>Departure Time</th>
-        <th>City Name</th>
-        <th>Code</th>
-        <th>Airline</th>
-        <th>Gate</th>
-        <th>Status</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="from-dark bg-gradient-to-r to-black text-white">
+    <table class="flex flex-col">
+      <thead class="from-light flex bg-gradient-to-r to-white text-black">
+        <tr class="container mx-auto grid grid-cols-10">
+          <th class="md:col-span-2">Departure Time</th>
+          <th class="md:col-span-2">City Name</th>
+          <th>Code</th>
+          <th class="md:col-span-2">Airline</th>
+          <th>Gate</th>
+          <th class="md:col-span-2">Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="container mx-auto grid grid-cols-10">
+          <td class="md:col-span-2">10:20</td>
+          <td class="md:col-span-2">Los Santos International</td>
+          <td>LSI</td>
+          <td class="md:col-span-2">Adios Airlines</td>
+          <td>A4</td>
+          <td class="md:col-span-2">Flight Closing</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <style lang="scss"></style>
