7e04ae89f8b8aa20633728d6b6379e9f7b3279fe
James Manning <jamesmanningr98@gmail.com>
2022-10-15 19:34:58 +0100

    Created departures store

--- src/stores/departures.ts
+++ src/stores/departures.ts
@@ -1,5 +1,35 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import Axios from 'axios'
+
+import type { Departure, DepartureApiResponse } from '@types'
+
+// If this were more complex, it would be best to split the API interactions into a service file
+const departuresApiUrl =
+  'https://6315ae3e5b85ba9b11e4cb85.mockapi.io/departures/Flightdata'
 
 export const useDeparturesStore = defineStore('departures', () => {
-  return {}
+  const departures = ref<Departure[]>([])
+
+  /**
+   * Fetches the departures from the API
+   * put them in the store
+   * and returns them
+   */
+  const fetchDepartures = async (): Promise<Departure[]> => {
+    let res
+    try {
+      res = await Axios.get<DepartureApiResponse>(departuresApiUrl)
+    } catch (err) {
+      throw new Error('Error fetching departures')
+    }
+
+    departures.value = res.data.allDepartures
+    return departures.value
+  }
+
+  return {
+    departures,
+    fetchDepartures,
+  }
 })
