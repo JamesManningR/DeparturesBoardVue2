90e279fe0c0a1cf76fb5c6274ce7ce3f5f88dc1f
James Manning <jamesmanningr98@gmail.com>
2022-10-15 19:40:18 +0100

    Added spacing to departured table

--- src/components/departures/DeparturesTable.vue
+++ src/components/departures/DeparturesTable.vue
@@ -3,8 +3,8 @@
 <template>
   <div class="from-dark bg-gradient-to-r to-black text-white">
     <table class="flex flex-col">
-      <thead class="from-light flex bg-gradient-to-r to-white text-black">
-        <tr class="container mx-auto grid grid-cols-10">
+      <thead class="from-light mb-3 flex bg-gradient-to-r to-white text-black">
+        <tr class="container mx-auto grid grid-cols-10 text-left">
           <th class="md:col-span-2">Departure Time</th>
           <th class="md:col-span-2">City Name</th>
           <th>Code</th>
@@ -14,12 +14,14 @@
         </tr>
       </thead>
       <tbody>
-        <tr class="container mx-auto grid grid-cols-10">
+        <tr
+          class="text-bold border-light container mx-auto mb-3 grid grid-cols-10 rounded-lg border-2"
+        >
           <td class="md:col-span-2">10:20</td>
-          <td class="md:col-span-2">Los Santos International</td>
+          <td class="text-primary md:col-span-2">Los Santos International</td>
           <td>LSI</td>
           <td class="md:col-span-2">Adios Airlines</td>
-          <td>A4</td>
+          <td class="text-primary">A4</td>
           <td class="md:col-span-2">Flight Closing</td>
         </tr>
       </tbody>
