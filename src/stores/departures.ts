982258344444248ad22dd97006d8d73cec6c7045
James Manning <jamesmanningr98@gmail.com>
2022-10-15 22:17:14 +0100

    Added select and update functionality

--- src/stores/departures.ts
+++ src/stores/departures.ts
@@ -9,6 +9,9 @@ const departuresApiUrl =
   'https://6315ae3e5b85ba9b11e4cb85.mockapi.io/departures/Flightdata'
 
 export const useDeparturesStore = defineStore('departures', () => {
+  /**
+   * Holds all departures fetched from the api
+   */
   const departures = ref<Departure[]>([])
 
   /**
@@ -28,8 +31,54 @@ export const useDeparturesStore = defineStore('departures', () => {
     return departures.value
   }
 
+  /**
+   * Selected departure used for editing form
+   */
+  const selectedDeparture = ref<Departure | null>(null)
+
+  /**
+   * Selects a departure from the departures list
+   * @param flightNumber The flight number of the departure to select
+   * @returns The selected departure or null if not found
+   * @throws Error if the departure is not found
+   * @throws Error if the departures list is empty
+   */
+  const selectDeparture = (flightNumber: string): Departure | null => {
+    if (departures.value.length === 0)
+      throw new Error('Departures list is empty')
+
+    const departure = departures.value.find(
+      departure => departure.flightNumber === flightNumber
+    )
+
+    if (!departure) throw new Error('Departure not found')
+
+    selectedDeparture.value = departure
+    return selectedDeparture.value
+  }
+
+  /**
+   * Updates a departure in the departures list
+   * @param departure The departure to update
+   * @returns The updated departure
+   * @throws Error if the departure is not found
+   */
+  const updateDeparture = (departure: Departure): Departure => {
+    const index = departures.value.findIndex(
+      d => d.flightNumber === departure.flightNumber
+    )
+
+    if (index === -1) throw new Error('Departure not found')
+
+    departures.value[index] = departure
+    return departure
+  }
+
   return {
     departures,
     fetchDepartures,
+    selectedDeparture,
+    selectDeparture,
+    updateDeparture,
   }
 })
